7cc527aabd2f64aafed0ab8ecd4685a17c9a3f88
yanglu <yanglucisco@163.com>
2025-09-04 19:47:25 +0800

    feat: 实现了前后端分离项目中使用oauth2 pkce模式

--- gateway/src/main/resources/static/index.js
+++ gateway/src/main/resources/static/index.js
@@ -2,17 +2,22 @@
 
 function init(){
     let isFromAuth = isFromAuthServer();
-    if(isFromAuth.r){
+    if(isFromAuth.r === true){
         //如果是从授权服务器跳转回来的，则开始获取token
 //        sessionStorage.setItem("token", "token");
-        exchangeToken();
+//        exchangeToken();
+        console.log(isFromAuth.code);
+        console.log('从授权服务器回调回来的');
+        exchangeToken(isFromAuth.code);
         loadContent();
     }
     else{
         //不是从授权服务器跳转回来的
         //如果没有获取到token，则转到授权服务器登录页面
             if(!sessionStorage.getItem("token")){
-                initiateAuthFlow();
+//                initiateAuthFlow();
+                    location.href = 'http://auth-server:9000/oauth2/authorize?response_type=code&client_id=pkce-client&scope=openid pkce&redirect_uri=http://auth-gateway:10000/index.html&code_challenge_method=S256&code_challenge=hQqHvGROSi0bvuXVAUXnSj1ZN1p1pDTpnKy5HZvvAso';
+
                 return;
         //            location.href = "http://www.bing.com.cn";
             }
@@ -66,16 +71,17 @@ function test1(){
 }
 
 function isFromAuthServer(){
+console.log('开始初始化');
 const queryString = window.location.search;
 const params = new URLSearchParams(queryString);
 
 const code = params.get('code'); // "alice"
-const state = params.get('state'); // "25"
+//const state = params.get('state'); // "25"
 if(code){
     return {
             r: true,
-            code: code,
-            state: state
+            code: code
+//            state: state
         }
     }
     return {
@@ -83,4 +89,4 @@ if(code){
     }
 }
 
-init2();
+init();
